68f21d750fe803d5ff75b736b3026e0642560f4a
Naman Sood <mail@nsood.in>
2024-03-31 16:10:46 -0400

    initial mathNEWS changes
    Signed-off-by: Naman Sood <mail@nsood.in>

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const config = require('./config.pii');
-const fs = require('fs');
-const readline = require('readline');
 const process = require('process');
+
+const die = (msg) => { console.error(msg); process.exit(1); }
+const usage = `usage: ${process.argv[0]} ${process.argv[1]} [lock|unlock] [machine-code]`;
+const action = process.argv[2];
+const machine = process.argv[3];
+
+if((action !== "lock" && action !== "unlock") || !machine) {
+	die(usage);
+}
 
 const client = new Client({
 	intents: [
@@ -13,61 +20,29 @@ const client = new Client({
 client.login(config.token);
 
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-
-	pollBrightness();
+	console.log(`Logged in as ${c.user.tag}`);
 	main();
 });
 
-let lightsOn = false;
-
-function pollBrightness() {
-	const file = fs.createReadStream(config.device);
-	const reader = readline.createInterface({
-		input: file,
-		terminal: false
-	});
-	
-	reader.on('line', line => {
-		if(line.length === 0) return;
-		const brightness = Number(line);
-		if(brightness >= config.threshold) {
-			lightsOn = true;
-		}
-		else {
-			lightsOn = false;
-		}
-	});
-
-	process.on('SIGTERM', () => {
-		process.exit('SIGKILL');
-	});
-}
-
 function main() {
-	let prevStatus = null;
-	
-	client.on('rateLimit', e => {
-		console.log(e);
-	});
+	const emoji = {
+		lock: '🔒',
+		unlock: '🔓',
+	}
+	const updated = false;
+	const servers = client.guilds.cache;
+	for(const [_sId, server] of servers) {
+		const channels = server.channels.cache;
 
-	setInterval(function() {
-		const status = lightsOn ? 'open ✅' : 'closed ❌';
-		if(status !== prevStatus) {
-			prevStatus = status;
-
-			const servers = client.guilds.cache;
-			for(const [_sId, server] of servers) {
-				const channels = server.channels.cache;
-				
-				//server.members.cache.get(client.user.id).setNickname(status);
-
-				for(const [_cId, channel] of channels) {
-					if(channel.name.indexOf('office-is') >= 0) {
-						channel.setName(`office-is-${status}`);
-					}
-				}
+		for(const [_cId, channel] of channels) {
+			if(channel.name.indexOf(machine) >= 0) {
+				channel.setName(`${machine}-${action}ed-${emoji[action]}`);
+				console.log(`channel updated in ${server.name}`);
+				updated = true;
 			}
 		}
-	}, 60 * 1000);
+	}
+	if(!updated) {
+		die('could not find channel');
+	}
 }
